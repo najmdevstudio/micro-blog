efad11c1a2f94c59cee5ea9805155d85d1381cca
najmdevstudio <najm.devops@gmail.com>
2023-02-25 15:15:37 +0530

    Corrected code to use codepoints API

--- src/main/java/MicroBlog.java
+++ src/main/java/MicroBlog.java
@@ -1,7 +1,10 @@
+
+
 class MicroBlog {
-    public String truncate(String input) {
-        if(input.length() > 5)
-           return input.substring(0,4);
+    public String truncate(String input)  {
+        int[] b1 = input.codePoints().toArray();  //codepoints api converting it into int array here
+        if(b1.length > 5)
+           return new String(b1,0,5);
         else
            return input;
     }
